f8e47e0bc57272f6c31d877e3e5456e96fbbab49
yangml <yangmingliang_22@163.com>
2014-09-01 21:52:58 +0800

    Myeclipse 2014 testing

---
+++ src/com/bling/test/My2014.java
@@ -0,0 +1,10 @@
+package com.bling.test;
+
+public class My2014 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Myeclipse 2014");
+	}
+
+}
